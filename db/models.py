d81d48ddcd20e88714324f6f570217de6be473f8
Vadym <kirilkovadim193@gmail.com>
2025-08-04 17:45:53 +0300

    Corrected imports

--- db/models.py
+++ db/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
 from django.db import models
 from django.db.models import IntegerField
-from datetime import datetime
 
 
 class Genre(models.Model):

395f87357b1bd9e2e6023af033de30e7b48477e4
Vadym <kirilkovadim193@gmail.com>
2025-08-05 17:04:37 +0300

    Refactored create_order function in services/order.py

--- services/order.py
+++ services/order.py
@@ -1,31 +1,31 @@
-from datetime import datetime
-from db.models import Order, Ticket, User, MovieSession
+from dateutil.parser import parse
 from django.db.models import QuerySet
+from db.models import Order, Ticket, User, MovieSession
 from django.db import transaction
 
 
 def create_order(
-        tickets: list[dict],
+        tickets: list[Ticket],
         username: str,
-        date: datetime = None
-) -> Order | None:
-    user = User.objects.get(username=username)
+        date: str = None
+) -> Order:
     with transaction.atomic():
-        order_info = {"user": user}
-        if date:
-            if isinstance(date, str):
-                date = datetime.strptime(date, "%Y-%m-%d %H:%M")
-            order_info["created_at"] = date
-        order = Order.objects.create(**order_info)
-        for ticket_info in tickets:
-            movie_session_id = ticket_info["movie_session"]
-            movie_session = MovieSession.objects.get(id=movie_session_id)
-            ticket_data_for_creating = ticket_info.copy()
-            ticket_data_for_creating.pop("movie_session")
+        user = User.objects.get(username=username)
+        order = Order.objects.create(
+            user=user
+        )
+        if date is not None:
+            order.created_at = parse(date)
+            order.save()
+        for ticket in tickets:
             Ticket.objects.create(
-                order=order,
-                movie_session=movie_session,
-                **ticket_data_for_creating)
+                movie_session=MovieSession.objects.get(
+                    id=ticket["movie_session"]
+                ),
+                row=ticket["row"],
+                seat=ticket["seat"],
+                order=order
+            )
         return order
 
 
